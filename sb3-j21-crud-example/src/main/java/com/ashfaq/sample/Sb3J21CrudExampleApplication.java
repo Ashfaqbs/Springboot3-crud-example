8e24b803ff69a6018889bae45140978fba371a3d
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2024-12-07 13:47:27 +0530

    to work with docker and Jenkins

---
+++ sb3-j21-crud-example/src/main/java/com/ashfaq/sample/Sb3J21CrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ashfaq.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3J21CrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3J21CrudExampleApplication.class, args);
+	}
+
+}
